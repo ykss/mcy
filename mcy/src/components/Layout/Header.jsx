ca87acb441ba32ff22f14cab98324d24481c0c2c
dony0720 <97350693+dony0720@users.noreply.github.com>
2024-02-28 10:27:01 +0900

    feat: Header 생성

--- mcy/src/components/Layout/Header.jsx
+++ mcy/src/components/Layout/Header.jsx
@@ -1,5 +1,33 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div
+      css={css({
+        color: "black",
+        fontWeight: "bold",
+        position: "fixed",
+        top: "10px",
+        right: "0",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        width:"100vw"
+
+        
+      })}
+    >
+      <div css={css({
+        margin:"0 auto"
+      })}>MCY</div>
+      <div>
+        <button css={css({
+          margin:"0 50px 0 0"
+        })}>햄버거 버튼</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
