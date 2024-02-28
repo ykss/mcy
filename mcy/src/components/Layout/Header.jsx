0cb3ef2dd78cefc2b655ed114efa05349b386e2a
dony0720 <97350693+dony0720@users.noreply.github.com>
2024-02-28 11:44:14 +0900

    fix: MCY 로고 정중앙 정렬

--- mcy/src/components/Layout/Header.jsx
+++ mcy/src/components/Layout/Header.jsx
@@ -8,23 +8,18 @@ const Header = () => {
         color: "black",
         fontWeight: "bold",
         position: "fixed",
+        borderBottom: "solid #FEEDCD",
         top: "10px",
         right: "0",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        width:"100vw"
-
-        
+        width: "100vw",
       })}
     >
-      <div css={css({
-        margin:"0 auto"
-      })}>MCY</div>
-      <div>
-        <button css={css({
-          margin:"0 50px 0 0"
-        })}>햄버거 버튼</button>
+      <div
+        css={css({
+          margin: "0 auto",
+        })}
+      >
+        MCY
       </div>
     </div>
   );
