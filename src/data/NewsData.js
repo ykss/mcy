2271777f2dba551efff222d6008a37b7992baef4
dony0720 <97350693+dony0720@users.noreply.github.com>
2024-04-01 11:08:48 +0900

    feat: News 페이지 생성 1. 제목 컴포넌트 생성 2. 소식 리스트 컴포넌트 생성 3. 소식 데이터 파일 생성

---
+++ src/data/NewsData.js
@@ -0,0 +1,97 @@
+export const NewsData = [
+  {
+    id: 0,
+    month: "01",
+    date: "07",
+  },
+  {
+    id: 1,
+    month: "01",
+    date: "14",
+  },
+  {
+    id: 2,
+    month: "01",
+    date: "21",
+  },
+  {
+    id: 3,
+    month: "01",
+    date: "28",
+  },
+  {
+    id: 4,
+    month: "02",
+    date: "04",
+  },
+  {
+    id: 5,
+    month: "02",
+    date: "11",
+  },
+  {
+    id: 6,
+    month: "02",
+    date: "18",
+  },
+  {
+    id: 7,
+    month: "02",
+    date: "25",
+  },
+  {
+    id: 8,
+    month: "03",
+    date: "03",
+  },
+  {
+    id: 9,
+    month: "03",
+    date: "10",
+  },
+  {
+    id: 10,
+    month: "03",
+    date: "17",
+  },
+  {
+    id: 11,
+    month: "03",
+    date: "24",
+  },
+  {
+    id: 12,
+    month: "03",
+    date: "31",
+  },
+  {
+    id: 13,
+    month: "04",
+    date: "07",
+  },
+  {
+    id: 14,
+    month: "04",
+    date: "14",
+  },
+  {
+    id: 15,
+    month: "04",
+    date: "21",
+  },
+  {
+    id: 16,
+    month: "04",
+    date: "28",
+  },
+  {
+    id: 17,
+    month: "05",
+    date: "05",
+  },
+  {
+    id: 18,
+    month: "05",
+    date: "12",
+  },
+]
