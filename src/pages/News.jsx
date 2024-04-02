5f17038ca6f3283f1fba1183dd2ab2feac7fc183
dony0720 <97350693+dony0720@users.noreply.github.com>
2024-04-02 15:17:05 +0900

    fix: 1차 피드백 반영 1. 월별로 리스트 조회 2. 날짜 변경 기능 3. 컴포넌트 변수명 개선

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,11 +1,30 @@
 import styled from "@emotion/styled"
-import Layout from "../components/Layout/Layout"
-import {Stack, Typography } from "@mui/material"
+import { Stack, Typography } from "@mui/material"
 import CampaignOutlinedIcon from "@mui/icons-material/CampaignOutlined"
+import Accordion from "@mui/material/Accordion"
+import AccordionSummary from "@mui/material/AccordionSummary"
+import AccordionDetails from "@mui/material/AccordionDetails"
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
+import React, { useEffect } from "react"
 
+import Layout from "../components/Layout/Layout"
 import Title from "../components/shared/Title"
 import { NewsData } from "../data/NewsData"
 const News = () => {
+  // 연도와 월 데이터 추출하여 중복 제거
+  const years = [...new Set(NewsData.map(item => item.year))]
+  const months = [...new Set(NewsData.map(item => item.month))]
+
+  const [selectedYear, setSelectedYear] = React.useState(years[0])
+  const [selectedMonth, setSelectedMonth] = React.useState(months[0])
+
+  const handleYearChange = event => {
+    setSelectedYear(event.target.value)
+  }
+
+  const handleMonthChange = event => {
+    setSelectedMonth(event.target.value)
+  }
   return (
     <Layout>
       <NewsWrapper>
@@ -13,19 +32,44 @@ const News = () => {
           <CampaignOutlinedIcon sx={{ fontSize: 40 }} />
           <Title>MCY 소식</Title>
         </TitleWrapper>
-        {/* <NewsListWrapper> */}
+        <SelectWrapper>
+          <SelectItem>
+            <select value={selectedYear} onChange={handleYearChange}>
+              {years.map((year, index) => (
+                <option key={index} value={year}>
+                  {year}년
+                </option>
+              ))}
+            </select>
+          </SelectItem>
+          <SelectItem>
+            <select value={selectedMonth} onChange={handleMonthChange}>
+              {months.map((month, index) => (
+                <option key={index} value={month}>
+                  {month}월
+                </option>
+              ))}
+            </select>
+          </SelectItem>
+        </SelectWrapper>
         <RenderingArea>
-          {NewsData.map(item => {
-            return (
-              <NewsList key={item.id}>
-                <NewsContent fontSize={14}>
-                  {item.month}월 {item.date}일 광고
-                </NewsContent>
-              </NewsList>
-            )
-          })}
+          {NewsData.filter(item => item.year === +selectedYear && item.month === selectedMonth).map(item => (
+            <NewsListWrapper key={item.id}>
+              <Accordion>
+                <AccordionSummary expandIcon={<ArrowDropDownIcon />} aria-controls="panel2-content">
+                  <NewsItem fontSize={16} fontWeight={"bold"}>
+                    {item.month}월 {item.date}일 광고
+                  </NewsItem>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    {item.month}월 {item.date}일 소식입니다
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </NewsListWrapper>
+          ))}
         </RenderingArea>
-        {/* </NewsListWrapper> */}
       </NewsWrapper>
     </Layout>
   )
@@ -43,25 +87,37 @@ const TitleWrapper = styled(Stack)`
   padding: 0 10px;
 `
 
+const SelectWrapper = styled(Stack)`
+  height: 10%;
+  width: 90%;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+`
+
+const SelectItem = styled(Stack)`
+  width: 30%;
+`
+
 const RenderingArea = styled(Stack)`
-  margin-top 10px;
-  height: 700px;
+  margin-top: 15px;
+  height: 500px;
   width: 100%;
   align-items: center;
   overflow-y: auto; /* 스크롤이 있는 경우만 스크롤바를 표시 */
   &::-webkit-scrollbar {
-    width: 0; 
+    width: 0;
   }
 `
 
-const NewsList = styled(Stack)`
-  width: 100%;
-  border-bottom: 2px solid #986c6c;
+const NewsListWrapper = styled(Stack)`
+  width: 90%;
+  margin: 10px 0;
   justify-content: center;
 `
-const NewsContent = styled(Stack)`
-  height: 50px;
-  margin-left: 10px;
+const NewsItem = styled(Stack)`
+  height: 60px;
+  margin-left: 2px;
   justify-content: center;
 `
 export default News
