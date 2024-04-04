402a46c8e5d9fcaf348a8a4222b49fa7b12d336d
Persimmon <53931787+DHyeonJ@users.noreply.github.com>
2024-04-04 17:55:21 +0900

    feat. 해당 날짜에 출석 데이터 저장 remove. rebase 과정 중 살아난 파일 삭제 fix. Main 파일 높이 styled로 통일

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,11 @@
-import { Stack } from "@mui/system";
-import Typography from "@mui/material/Typography";
-import { Chip, styled } from "@mui/material";
-import Layout from "../components/Layout/Layout";
-import CarouselSlider from "../components/shared/CarouselSlider";
-import mainSlider1 from "../assets/images/mainSlider1.webp";
-import mainSlider2 from "../assets/images/mainSlider2.webp";
-import mainSlider3 from "../assets/images/mainSlider3.webp";
+import { Stack } from "@mui/system"
+import Typography from "@mui/material/Typography"
+import { Chip, styled } from "@mui/material"
+import Layout from "../components/Layout/Layout"
+import CarouselSlider from "../components/shared/CarouselSlider"
+import mainSlider1 from "../assets/images/mainSlider1.webp"
+import mainSlider2 from "../assets/images/mainSlider2.webp"
+import mainSlider3 from "../assets/images/mainSlider3.webp"
 
 const Main = () => {
   const McyImgs = [
@@ -18,7 +18,7 @@ const Main = () => {
     {
       img: mainSlider3,
     },
-  ];
+  ]
 
   return (
     <Layout>
@@ -26,7 +26,7 @@ const Main = () => {
         <CarouselWrapper>
           <CarouselSlider imageArray={McyImgs} />
         </CarouselWrapper>
-        <ContentWrapper sx={{ height: "20%" }}>
+        <ContentWrapper>
           <Chip
             label="교육 표어"
             color="primary"
@@ -37,12 +37,10 @@ const Main = () => {
               color: "#000",
             }}
           />
-          <Typography variant="body1">
-            예수그리스도의 영이 다시 살아나게 하소서
-          </Typography>
+          <Typography variant="body1">예수그리스도의 영이 다시 살아나게 하소서</Typography>
           <Typography variant="overline">시 51:10, 출23:25, 슥4:6</Typography>
         </ContentWrapper>
-        <ContentWrapper sx={{ height: "30%" }}>
+        <ContentWrapper>
           <Chip
             label="교육 목표"
             color="primary"
@@ -53,38 +51,33 @@ const Main = () => {
               color: "#000",
             }}
           />
-          <Typography variant="body1">
-            1. 예배로 온전한 신앙을 회복하자.
-          </Typography>
-          <Typography variant="body1">
-            2. 기도로 상한 마음을 회복하자.
-          </Typography>
-          <Typography variant="body1">
-            3. 교제와 셀 모임을 통해 관계 회복하자.
-          </Typography>
+          <Typography variant="body1">1. 예배로 온전한 신앙을 회복하자.</Typography>
+          <Typography variant="body1">2. 기도로 상한 마음을 회복하자.</Typography>
+          <Typography variant="body1">3. 교제와 셀 모임을 통해 관계 회복하자.</Typography>
         </ContentWrapper>
       </MainWrapper>
     </Layout>
-  );
-};
+  )
+}
 
 const MainWrapper = styled(Stack)`
   width: 100vw;
   height: calc(100dvh - 120px);
   align-items: center;
-`;
+`
 
 const CarouselWrapper = styled(Stack)`
   justify-content: center;
   align-items: center;
   height: 50%;
   width: 100%;
-`;
+`
 
 const ContentWrapper = styled(Stack)`
   width: 80%;
+  height: 30%;
   align-items: flex-start;
   justify-content: space-evenly;
-`;
+`
 
-export default Main;
+export default Main
