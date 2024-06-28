bd84ea5d643a597a41f749d3d1c5fdc98f49f89c
dony0720 <97350693+dony0720@users.noreply.github.com>
2024-06-28 17:23:05 +0900

    feat: 생일 페이지 리뉴얼

--- src/components/shared/PlusButton.jsx
+++ src/components/shared/PlusButton.jsx
@@ -1,4 +1,4 @@
-export const PlusButton = () => {
+const NewsPlusButton = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="none" viewBox="0 0 35 35">
       <rect width="34" height="34" x="0.5" y="0.5" fill="#B4DFC3" stroke="black" rx="12.5" />
@@ -6,3 +6,13 @@ export const PlusButton = () => {
     </svg>
   )
 }
+const BirthdayPlusButton = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="none" viewBox="0 0 35 35">
+      <rect width="34" height="34" x="0.5" y="0.5" fill="#F3C5C5" stroke="black" rx="12.5" />
+      <path stroke="#404040" strokeLinecap="round" strokeWidth="3" d="M12.4999 17.3033L23.1065 17.3033M17.8032 12L17.8032 22.6066" />
+    </svg>
+  )
+}
+
+export { NewsPlusButton, BirthdayPlusButton }
