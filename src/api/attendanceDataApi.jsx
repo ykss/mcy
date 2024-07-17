e98d17186bd4bc1bc6cef6a552f6053168f582c4
전대현 <persimmon@persimmon-MacBook-Pro.local>
2024-07-17 19:50:53 +0900

    fix. 출석 데이터 api 수정 fix. meta image 경로 수정 fix. 다양한 icon 추가 fix. 출석 데이터 Id추가로 데이터 없을 때는 추가 있을 때는 수정으로 변경

--- src/api/attendanceDataApi.jsx
+++ src/api/attendanceDataApi.jsx
@@ -1,6 +1,7 @@
-import { collection, getDocs } from "firebase/firestore"
+import { collection, getDocs, getDoc, doc, updateDoc, setDoc } from "firebase/firestore"
 import { db } from "../firebase"
 
+// MCY member 불러오기
 const getAttendanceApi = async () => {
   try {
     const attendanceCollection = collection(db, "attendanceData")
@@ -15,4 +16,23 @@ const getAttendanceApi = async () => {
   }
 }
 
-export { getAttendanceApi }
+// 출석 데이터 수정하기
+const updateAttendanceApi = async (id, attendanceData) => {
+  try {
+    const docRef = doc(db, "attendanceData", id)
+    const docSnap = await getDoc(docRef)
+
+    // 문서가 이미 존재하는 경우 업데이트, 존재하지 않는 경우 새로 생성
+    if (docSnap.exists()) {
+      await updateDoc(docRef, attendanceData)
+      console.log("Document updated successfully!")
+    } else {
+      await setDoc(docRef, attendanceData)
+      console.log("New document created!")
+    }
+  } catch (error) {
+    console.error("Error updating document: ", error)
+    throw error // Optionally rethrow the error for handling in the caller function
+  }
+}
+export { getAttendanceApi, updateAttendanceApi }
