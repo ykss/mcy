c5cc782b3757208556d4197edc35c7713492d13a
Jungseok Shin <jsjb7164@naver.com>
2025-04-17 17:48:30 +0900

    style : Footer, 생일자 리스트 렌더링 영역  높이 수정

--- src/pages/BirthDay.tsx
+++ src/pages/BirthDay.tsx
@@ -4,28 +4,9 @@ import BirthDayList from "../components/BirthDay/BirthDayList"
 import { useState, useEffect } from "react"
 
 const BirthDay = () => {
-  const [containerHeight, setContainerHeight] = useState("75%")
-
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     if (window.innerHeight >= 700) {
-  //       setContainerHeight("75%")
-  //     } else {
-  //       setContainerHeight("75%")
-  //     }
-  //   }
-
-  //   handleResize()
-  //   window.addEventListener("resize", handleResize)
-
-  //   return () => {
-  //     window.removeEventListener("resize", handleResize)
-  //   }
-  // }, [])
-
   return (
     <Layout>
-      <div className="w-full h-full flex flex-col justify-center">
+      <div className="w-full h-full flex flex-col">
         {/* 월 선택 버튼 */}
         <div className="w-[90%] h-[30%] mx-auto flex flex-col justify-center gap-[20px]">
           <div className="text-2xl font-bold">월 선택</div>
@@ -42,8 +23,8 @@ const BirthDay = () => {
         </div>
 
         {/* 생일 리스트 */}
-        <div className="w-[90%] h-[60%] mt-[20px] mx-auto bg-[#F3C5C5] rounded-[17px] border border-solid border-black box-border flex flex-col justify-center ">
-          <div className="w-[90%] h-[10%] mx-auto text-xl font-bold py-3">3월 생일을 축하합니다</div>
+        <div className="w-[90%] h-[70%] mt-[20px] mx-auto bg-[#F3C5C5] rounded-[17px] border border-solid border-black box-border flex flex-col justify-center gap-1">
+          <div className="w-[90%] h-[10%] mx-auto text-xl font-bold">3월 생일을 축하합니다</div>
           <div
             className="bg-[#F0F0F0] w-[90%] h-[75%] mx-auto rounded-[17px] border border-solid border-black grid grid-cols-2 gap-6 justify-items-center items-center box-border p-4 py-6 overflow-y-scroll"
             style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}>
