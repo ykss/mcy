c7157de9109625847f0f8692a97f80a238f1fe3a
DHyeonJ <eogus724@naver.com>
2025-04-24 18:15:53 +0900

    design. menuDrawer mui -> shadcn and styled-componts -> tailwindcss change
    rename. js파일 -> ts 변환(메뉴드로어,handle previousWeek files)

--- src/components/Layout/MenuDrawer.tsx
+++ src/components/Layout/MenuDrawer.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Drawer, DrawerContent, DrawerHeader, DrawerFooter } from "../../components/ui/drawer"
+import { Sheet, SheetContent, SheetHeader, SheetFooter } from "../../components/ui/sheet"
 import { MenuDrawerProps } from "../../types/MenuDrawer"
 import PAGE_PATH from "../../constants/path"
 import { X } from "lucide-react"
@@ -20,15 +20,15 @@ const MenuDrawer = ({ open, setOpen, navigate }: MenuDrawerProps) => {
   }
 
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerContent>
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetContent>
         <div className="absolute right-1 top-3 text-black">
           <X className="text-[15px]" onClick={toggleDrawer} />
         </div>
-        <DrawerHeader className="mt-[60px]">
+        <SheetHeader className="mt-[60px]">
+          {/* 현재 사용하지 않는 메뉴 */}
           <div className="w-[100%]">
-            {/* 현재 사용하지 않는 메뉴 */}
-            {/* <div className={"ml-[10px]"} onClick={() => handleNavigate(PAGE_PATH.NEWS)}>
+            <div className={"ml-[10px]"} onClick={() => handleNavigate("PAGE_PATH.NEWS")}>
               <div className="flex flex-row w-full">
                 <div className="flex justify-center items-center w-[30%]">
                   <div className="w-[90%] h-[30%] bg-[#c7bdeb] border-[1px] border-solid  border-black rounded-[22px]" />
@@ -37,7 +37,7 @@ const MenuDrawer = ({ open, setOpen, navigate }: MenuDrawerProps) => {
                   <p className=" font-['Noto_Sans'] font-semibold text-[24px] ">소식</p>
                 </div>
               </div>
-            </div> */}
+            </div>
 
             <div className={"ml-[10px]"} onClick={() => handleNavigate(admin ? "attendance" : "attendanceStatus")}>
               <div className="flex flex-row w-[100%]">
@@ -49,7 +49,7 @@ const MenuDrawer = ({ open, setOpen, navigate }: MenuDrawerProps) => {
                 </div>
               </div>
             </div>
-            <div className={"ml-[10px]"} onClick={() => handleNavigate(PAGE_PATH.BIRTHDAY)}>
+            <div className={"ml-[10px]"} onClick={() => handleNavigate("birthday")}>
               <div className="flex flex-row w-full">
                 <div className="flex justify-center items-center w-[30%]">
                   <div className="w-[90%] h-[30%] bg-[#f3c5c5] border-[1px] border-solid  border-black rounded-[22px]" />
@@ -60,9 +60,9 @@ const MenuDrawer = ({ open, setOpen, navigate }: MenuDrawerProps) => {
               </div>
             </div>
           </div>
-        </DrawerHeader>
+        </SheetHeader>
 
-        <DrawerFooter className="flex flex-row justify-end gap-2 mr-[23px] mt-[160px]">
+        <SheetFooter className="flex flex-row justify-end gap-2 mr-[23px] mt-[160px]">
           <div
             className="font-['Noto_Sans'] font-semibold text-[16px] text-black"
             onClick={() => {
@@ -70,9 +70,9 @@ const MenuDrawer = ({ open, setOpen, navigate }: MenuDrawerProps) => {
             }}>
             {admin ? "로그아웃" : "로그인"}
           </div>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   )
 }
 
