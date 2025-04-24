242ed8897fa7d57cc8f0aa5af39e7e21ce3b7942
Jungseok Shin <jsjb7164@naver.com>
2025-04-24 17:30:27 +0900

    docs : 불필요한 코드 정리 및 출석체크 페이지 생성

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -2,6 +2,7 @@ const PAGE_PATH = {
   LOGIN: "/",
   MAIN: "/main",
   BIRTHDAY: "/birthDay",
+  ATTENDANCE_CHECK: "/attendanceCheck",
 } as const
 
 export default PAGE_PATH
---
+++ src/pages/AttendanceCheck.tsx
@@ -0,0 +1,16 @@
+import dayjs from "dayjs"
+import "dayjs/locale/ko"
+
+import Layout from "../components/Layout/Layout"
+
+const AttendanceCheck = () => {
+  return (
+    <div className="w-full h-[100dvh]">
+      <Layout>
+        <div></div>
+      </Layout>
+    </div>
+  )
+}
+
+export default AttendanceCheck
